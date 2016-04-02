a33d3b20ac60e9d4f5f0708c53c97629e86b7e53
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-04-02 15:20:41 +0200

    Initial DarkKat bring up:
    - Add dk_bullhead build target
    - Add inline kernel build
    - Change default wallpaper
    - Add Android 6 bootanimation
    - Disable forced encryption
    - Disable device verification of /system /vendor
    - Override build id to shutup verity
    
    Inline kernel build for 64 bit kernel:
    Based on OmniRom:
    bullhead: build 64-bit kernel:
    https://github.com/omnirom/android_device_lge_bullhead/commit/2d50d455fc4f660fb3ae4058fd31fc74e1b04f0f
    
    Disable device verification of /system /vendor:
    base on PureNexus, (beanstown106):
    bullhead: disable device verification of /system /vendor:
    https://github.com/PureNexusProject/android_device_lge_bullhead/commit/a4c47e54a93548c935135832be124b2e3bb3a313
    
    Override build id to shutup verity:
    Based on OmniRom:
    bullhead: Override build id to shutup verity:
    https://github.com/omnirom/android_device_lge_bullhead/commit/7427421bcb2caeb6220f4e74c96fc51809687428
    
    bullhead: update fingerprint and description for MMB29V:
    https://github.com/omnirom/android_device_lge_bullhead/commit/0b86de903ffc81a0dabcabb75a5dff5023b27985

---
+++ dk_bullhead.mk
@@ -0,0 +1,65 @@
+#
+# Copyright 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/lge/bullhead/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the darkkat vendor common configuration
+$(call inherit-product, vendor/dk/config/common.mk)
+
+PRODUCT_NAME := dk_bullhead
+PRODUCT_DEVICE := bullhead
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Nexus 5X
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0.1/MHC19J/2595691:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MHC19J 2595691 release-keys" \
+    BUILD_ID=MHC19J
+
+# Kernel inline build
+TARGET_KERNEL_SOURCE := kernel/msm/bullhead
+TARGET_KERNEL_CONFIG := bullhead_defconfig
+TARGET_VARIANT_CONFIG := bullhead_defconfig
+TARGET_SELINUX_CONFIG := bullhead_defconfig
+BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_USES_UNCOMPRESSED_KERNEL := true
+
+# Enable vendor image symlink
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# Bootanimation
+PRODUCT_COPY_FILES += \
+  device/lge/hammerhead/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
+$(call inherit-product, device/lge/bullhead/device.mk)
+$(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3
+
